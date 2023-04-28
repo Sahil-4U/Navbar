1a11ca3152c82f588d34400a5e5464468c26fc46
Sahil <ss6611786@gmail.com>
2023-04-28 13:06:30 +0530

    some comments added

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ const bottomContainerEl=document.querySelector('.bottom-container');
 
 window.addEventListener("scroll",()=>{
   if(window.scrollY > bottomContainerEl.offsetTop-navbarEl.offsetHeight-10){
-    console.log(bottomContainerEl.offsetTop+"ye to bottom container ka top");
-    console.log(navbarEl.offsetHeight+"ye h navbar ki height");
-    console.log(window.scrollY +'ye h vo point jha dono mile')
+    // console.log(bottomContainerEl.offsetTop+"ye to bottom container ka top");
+    // console.log(navbarEl.offsetHeight+"ye h navbar ki height");
+    // console.log(window.scrollY +'ye h vo point jha dono mile')
     navbarEl.classList.add("active");
   }else{
     navbarEl.classList.remove("active");
