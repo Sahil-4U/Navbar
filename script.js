bd4d8b4ec65f79bda8cec31466e80d000245da9c
Sahil <ss6611786@gmail.com>
2023-04-28 13:02:58 +0530

    here i have completed the js file and our project is completed

---
+++ script.js
@@ -0,0 +1,13 @@
+const navbarEl=document.querySelector('.navbar');
+const bottomContainerEl=document.querySelector('.bottom-container');
+
+window.addEventListener("scroll",()=>{
+  if(window.scrollY > bottomContainerEl.offsetTop-navbarEl.offsetHeight-10){
+    console.log(bottomContainerEl.offsetTop+"ye to bottom container ka top");
+    console.log(navbarEl.offsetHeight+"ye h navbar ki height");
+    console.log(window.scrollY +'ye h vo point jha dono mile')
+    navbarEl.classList.add("active");
+  }else{
+    navbarEl.classList.remove("active");
+  }
+})
